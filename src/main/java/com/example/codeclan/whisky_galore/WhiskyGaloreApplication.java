0e4095a338be7fe8e330112c0e86a783508c9d76
Kurt S Steffan <kurt.s.steffan@gmail.com>
2021-02-10 15:18:59 +0000

    Initial commit

---
+++ src/main/java/com/example/codeclan/whisky_galore/WhiskyGaloreApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.whisky_galore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiskyGaloreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiskyGaloreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codeclan/whisky_galore/WhiskyGaloreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.whisky_galore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiskyGaloreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
